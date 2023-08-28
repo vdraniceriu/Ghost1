b7c6b4b83dde52846522d614ed33c7810d8906e0
Mihai-Vlad Draniceriu <vlad.draniceriu@fortech.ro>
2023-08-28 17:40:30 +0300

    added tests

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,16 @@
+describe('GhostOpen', () => {
+  it('passes', () => {
+    cy.visit('/')
+    cy.contains('Resources').click();
+    cy.contains('Start here').click();
+    cy.get('[name= "search"]').type('create new blog');
+    cy.get('li').eq(9).click();
+    cy.scrollTo('top');
+    cy.contains('Pricing').click();
+    cy.get('[data-price="starter"]').invoke('text').then(starterPrice => {
+    cy.get('[id="members"]').invoke('val', 8).trigger('input')
+    cy.get('[data-price="starter"]').invoke('text').then(parseFloat).should('be.gt',parseFloat(starterPrice) )
+    cy.get('[data-price="creator"]').invoke('text').then(parseFloat).should('be.gt', 25 )
+    })
+  })
+  })
