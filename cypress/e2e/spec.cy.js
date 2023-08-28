77bb5f44ffca04f0f582455562908034cda968d7
Mihai-Vlad Draniceriu <vlad.draniceriu@fortech.ro>
2023-08-28 20:18:34 +0300

    added last validations

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -11,6 +11,8 @@ describe('GhostOpen', () => {
     cy.get('[id="members"]').invoke('val', 8).trigger('input')
     cy.get('[data-price="starter"]').invoke('text').then(parseFloat).should('be.gt',parseFloat(starterPrice) )
     cy.get('[data-price="creator"]').invoke('text').then(parseFloat).should('be.gt', 25 )
+    cy.get('[data-price="team"]').invoke('text').then(parseFloat).should('be.gt', 50 )
+    cy.get('[data-price="business"]').invoke('text').then(parseFloat).should('be.gt', 199 )
     })
   })
   })
